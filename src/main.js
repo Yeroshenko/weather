d201c247036d8e6f8456596bc166280769152fa1
personalj <popelaleksandr@gmail.com>
2022-05-28 16:10:17 +0300

    feat: plugins

--- src/main.js
+++ src/main.js
@@ -2,19 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
-import VueToast from "vue-toast-notification";
-import "vue-toast-notification/dist/theme-sugar.css";
-
-import Vuesax from 'vuesax';
-import 'vuesax/dist/vuesax.css';
-
-import 'material-icons/iconfont/material-icons.css';
-
-Vue.use(VueToast);
-Vue.use(Vuesax);
+import initVue from '@/plugins/initVue';
 
 Vue.config.productionTip = false;
+
+initVue(Vue);
 
 new Vue({
   router,
